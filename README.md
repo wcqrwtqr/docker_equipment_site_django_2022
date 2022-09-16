c67ca54a84590c130f0b98f3a3c2ede8a10aa64c
wcqrwtqr <mohalbatati@hotmail.com>
2022-09-16 23:24:04 +0300

    first commit and making my web page on docker

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # docker_equipment_site_django_2022
+
 Make a web page with postgres database to track equipment and jobs
+
+To build the docker-compose file use the below code
+
+``` bash
+docker-compose up -d --build
+```
+
+A container called (web) will be created along with postgres database  and the web page will be running
+
+To do migrations and makemigrations use the command below
+
+``` bash
+docker-compose exec web python manage.py migrate
+
+docker-compose exec web python manage.py makemigrations
+```
+
+To start and stop the container use the below command
+
+``` bash
+docker-compose up -d 
+
+docker-compose down
+```
---
+++ equipmentList/views.py
@@ -0,0 +1,71 @@
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+# from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import  login_required
+from django.urls import reverse_lazy
+from django import forms
+from .forms import EquipmentForm
+from . import models
+from django.contrib.auth.mixins import PermissionRequiredMixin
+# from django.db.models import Q
+# from equipmentMaintenance.models import MaintenanceDB
+# from .filters import EquipmentFilter, EquipmentMaintenanceFilter
+# from django.http import HttpResponse
+from django.template.loader import get_template
+# from xhtml2pdf import pisa
+
+
+class EquipmentListView(ListView):
+
+    template_name = 'equipmentList/equipment_page.html'
+    queryset = models.EQUIPMENT_DB.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['filter'] = EquipmentFilter(self.request.GET, queryset=self.queryset)
+        return context
+
+# # Getting the equipment vs maintenance views and filtering them
+# class EquipmentMaintenanceListView(ListView):
+#     template_name = 'equipmentList/equipment_maintenance_detail.html'
+#     queryset = MaintenanceDB.objects.all()
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['filter'] = EquipmentMaintenanceFilter(self.request.GET, queryset=self.queryset)
+#         return context
+
+class EquipmentDetailView(DetailView):
+    template_name = 'equipmentList/equipment_detail.html'
+    queryset = models.EQUIPMENT_DB.objects.all()
+    context_object_name = 'equipment_detail'
+
+
+class EquipmentCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = ("is_superuser")
+    template_name = 'equipmentList/equipment_new.html'
+    form_class = EquipmentForm
+    model = models.EQUIPMENT_DB
+    success_url = reverse_lazy('equipment')
+
+    def from_valid(self,form):
+        '''
+        Validate the form before saving
+        '''
+        self.object = form.save(commit = True)
+        self.object = save()
+        return super().form_valid(form)
+
+
+class EquipmentDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = ("is_superuser", )
+    model = models.EQUIPMENT_DB
+    success_url = reverse_lazy('equipment')
+    template_name = 'equipmentList/equipment_confirm_delete.html'
+
+
+class EquipmentUpdateView(UpdateView):
+    model = models.EQUIPMENT_DB
+    fields = '__all__'
+    template_name = 'equipmentList/equipment_update.html'
+    success_url = reverse_lazy('equipment')
+
