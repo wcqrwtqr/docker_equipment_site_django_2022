1c8a8f2b3aa46085fe36a211fe2574446cc0fff2
wcqrwtqr <mohalbatati@hotmail.com>
2022-10-19 10:46:29 +0300

    finally made the dashboard for equipment the right way

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from builtins import print
 from django.views.generic import TemplateView
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.db.models import Avg, Max, Min, FloatField, Count, IntegerField, Sum
@@ -21,15 +22,56 @@ class DashboardQueryOne(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['q1'] = db.objects.all().order_by('asset_num')[:10]
-        # The bad way TODO, need to get a better action for querying over the data
-        context['qs_av_KIUSLS'] = db.objects.filter(BL='SLS', BU='KIU').aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
-        context['qs_av_KIUSWT'] = db.objects.filter(BL='SWT', BU='KIU').aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
-        context['qs_av_SIUSLS'] = db.objects.filter(BL='SLS', BU='SIU').aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
-        context['qs_av_SIUSWT'] = db.objects.filter(BL='SWT', BU='SIU').aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
-        context['qs_av_SIUWHM'] = db.objects.filter(BL='WHM', BU='SIU').aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
-        context['qs_av_SIUSLS'] = db.objects.filter(BL='SLS', BU='SIU').aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
-        context['qs_av_SIUSWT'] = db.objects.filter(BL='SWT', BU='SIU').aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
-        context['qs_av_SIUWHM'] = db.objects.filter(BL='WHM', BU='SIU').aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
+
+        distinct_bl = db.objects.all().values_list('BL', flat=True).distinct()
+        distinct_bu = db.objects.all().values_list('BU', flat=True).distinct()
+        # Removing all the dublicates distinct values and put them in a set
+        mybl = set(distinct_bl)
+        mybu = set(distinct_bu)
+        # =========== Getting the avg cost for equipments =========================
+        equipmenet_cost_avg_dict = {}
+        for bar in mybu:
+            for baz in mybl:
+                text = bar + '_' + baz
+                x = db.objects.filter(BL=baz, BU=bar).aggregate(av=Avg('acquisition_cost', output_field=IntegerField()))
+                if x['av'] is None:
+                    # If the value is None then skip this point and move to the next one
+                    continue
+                equipmenet_cost_avg_dict[text] = x
+        context['all_equipment_blbu_avg'] = equipmenet_cost_avg_dict
+        # =========== Getting the avg nbv for equipments =========================
+        equipmenet_nbv_avg_dict = {}
+        for bar in mybu:
+            for baz in mybl:
+                text = bar + '_' + baz
+                x = db.objects.filter(BL=baz, BU=bar).aggregate(av=Avg('nbv', output_field=IntegerField()))
+                if x['av'] is None:
+                    # If the value is None then skip this point and move to the next one
+                    continue
+                equipmenet_nbv_avg_dict[text] = x
+        context['all_equipment_nvb_blbu_avg'] = equipmenet_nbv_avg_dict
+        # =========== Getting the sum aqcuistion for equipments =========================
+        equipmenet_acq_sum_dict = {}
+        for bar in mybu:
+            for baz in mybl:
+                text = bar + '_' + baz
+                x = db.objects.filter(BL=baz, BU=bar).aggregate(sum=Sum('acquisition_cost', output_field=IntegerField()), count=Count('acquisition_cost'))
+                if x['sum'] is None:
+                    continue
+                equipmenet_acq_sum_dict[text] = x
+        context['all_equipment_acq_blbu_sum'] = equipmenet_acq_sum_dict
+        # =========== Getting the sum NBV for equipments =========================
+        equipmenet_nbv_sum_dict = {}
+        for bar in mybu:
+            for baz in mybl:
+                text = bar + '_' + baz
+                x = db.objects.filter(BL=baz, BU=bar).aggregate(sum=Sum('nbv', output_field=IntegerField()), count=Count('nbv'))
+                if x['sum'] is None:
+                    continue
+                equipmenet_nbv_sum_dict[text] = x
+        print(equipmenet_nbv_sum_dict)
+        context['all_equipment_nbv_blbu_sum'] = equipmenet_nbv_sum_dict
+
         return context
 
 class DashboardQueryMain(ListView):
